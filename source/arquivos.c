f6a4d9adbe53447fd5a3ceeac93cde8209dfbde4
Davi A. M. da Silva <daviamsilva@hotmail.com>
2020-06-28 19:22:14 -0300

    Pequenas mudanças

--- source/arquivos.c
+++ source/arquivos.c
@@ -3,10 +3,10 @@
 
 FILE *abrir_arquivo(const char *msg)
 {
-    OPENFILENAMEA ofn = {0};
+    OPENFILENAME ofn = {0};
     char buffer[100] = {0};
 
-    ofn.lStructSize  = sizeof(OPENFILENAMEA);
+    ofn.lStructSize  = sizeof(OPENFILENAME);
     ofn.lpstrFilter  = "Arquivo de Texto\0*.txt\0";
     ofn.nMaxFile     = 100;
     ofn.lpstrTitle   = msg;
@@ -14,7 +14,7 @@ FILE *abrir_arquivo(const char *msg)
     ofn.lpstrDefExt  = "txt";
     ofn.Flags        = OFN_DONTADDTORECENT | OFN_FILEMUSTEXIST;
 
-    if (GetOpenFileNameA(&ofn))
+    if (GetOpenFileName(&ofn))
         return fopen(ofn.lpstrFile, "r");
     else
         return NULL;
@@ -22,10 +22,10 @@ FILE *abrir_arquivo(const char *msg)
 
 FILE *salvar_arquivo(const char *msg)
 {
-    OPENFILENAMEA ofn = {0};
+    OPENFILENAME ofn = {0};
     char buffer[100] = {0};
 
-    ofn.lStructSize  = sizeof(OPENFILENAMEA);
+    ofn.lStructSize  = sizeof(OPENFILENAME);
     ofn.lpstrFilter  = "Arquivo de Texto\0*.txt\0";
     ofn.nMaxFile     = 100;
     ofn.lpstrTitle   = msg;
@@ -33,7 +33,7 @@ FILE *salvar_arquivo(const char *msg)
     ofn.lpstrDefExt  = "txt";
     ofn.Flags        = OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
 
-    if (GetSaveFileNameA(&ofn))
+    if (GetSaveFileName(&ofn))
         return fopen(ofn.lpstrFile, "w");
     else
         return NULL;
